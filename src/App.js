0a76c69a4374531284c6b206aa4e6dcdc925a069
Munir ahamed <88591916+Munir-Ahamed@users.noreply.github.com>
2022-03-11 00:03:40 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app">
+      
+      <main>
+        <div className="search_box">
+          <input type="text" placeholder="Search..."/>
+        </div><br></br>
+        <div className="content">
+          <div className="city">trichy</div>
+          <div className="temp">15</div>
+          <div className="date">67</div>
+        </div>
+      </main>
+
+    </div>
+  );
+}
+
+export default App;
